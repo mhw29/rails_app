a801b88a07477c03da3fa25d3e9cffe63cf19584
Matt Williamson <matt.williamson@cdw.com>
2024-05-08 13:30:27 -0500

    Adding initial terraform and github action flow

---
+++ terraform/main.tf
@@ -0,0 +1,66 @@
+resource "aws_ecr_repository" "main" {
+  name = var.name
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_iam_policy_document" "github_actions" {
+  statement {
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart",
+    ]
+    resources = [aws_ecr_repository.main.arn]
+  }
+
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "github_actions" {
+  name        = "github-actions-${var.name}"
+  description = "Grant Github Actions the ability to push to ${var.name} from explosion/${var.name}"
+  policy      = data.aws_iam_policy_document.github_actions.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions.arn
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [resource.aws_iam_openid_connect_provider.arn]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.organization}/${var.name}:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "github-actions-${var.organization}-${var.name}"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "name" {
+  description = "Identifier for the application"
+  default     = "rails_app"
+}
+
+variable "organization" {
+  description = "Github organization"
+  default     = "mhw29"
+}
